31d7469781899f835668bcd39060181cc7cf2a0e
Oleg Bobok <bobizma@gmail.com>
2014-11-27 15:53:31 +0300

    yet another test and fix

--- lib/redeemer/integrations.rb
+++ lib/redeemer/integrations.rb
@@ -1,3 +1,3 @@
 Dir["#{File.dirname(__FILE__)}/integrations/*.rb"].sort.each do |path|
-  require "state_machine/integrations/#{File.basename(path)}"
+  require "redeemer/integrations/#{File.basename(path)}"
 end
