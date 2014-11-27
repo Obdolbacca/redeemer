676021cd535a7e1597b799b45aacb7d15d1e0a67
Oleg Bobok <bobizma@gmail.com>
2014-11-27 17:31:59 +0300

    v0.0.18

--- lib/redeemer/macro_methods.rb
+++ lib/redeemer/macro_methods.rb
@@ -3,15 +3,15 @@ require 'redeemer/redirector'
 module Redeemer
   module MacroMethods
     def ensure_secure_protocol(options = {})
-      self.to_secure(self, options)
+      self.to_secure(options)
     end
 
     def ensure_http(options = {})
-      Redeemer::Redirector.to_http(self, options)
+      Redeemer::Redirector.to_http(options)
     end
 
     def keep_current_protocol(options = {})
-      Redeemer::Redirector.remove_callbacks(self)
+      Redeemer::Redirector.remove_callbacks
     end
   end
 end
